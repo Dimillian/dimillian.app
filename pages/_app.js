345ace581fd0a631746487b5670112d7d6696172
Thomas Ricouard <ricouard77@gmail.com>
2023-08-22 12:53:42 +0200

    Remove RSS & more

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,6 @@ export default function Nextra({ Component, pageProps }) {
     <>
       <Head>
         <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-        <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
           as="font"
