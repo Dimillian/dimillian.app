4b20bcd0d3f4b67c529ad5bbe23470998ad4583b
Thomas Ricouard <ricouard77@gmail.com>
2023-08-23 06:33:23 +0200

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Nextra({ Component, pageProps }) {
     return (
       <>
         <Component {...pageProps} />
+        <Analytics />
       </>
     )
   }
