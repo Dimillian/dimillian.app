08df125ff593956f297b9f9ee7a3ee79f827030a
Thomas Ricouard <ricouard77@gmail.com>
2025-01-15 11:43:56 +0100

    Add speed insight

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Nextra({ Component, pageProps }) {
     return (
       <>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </>
     )
   }
